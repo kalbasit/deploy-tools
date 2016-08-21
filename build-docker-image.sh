259379ec17221f865daaf4e1e2a47168251fc2e3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2016-08-20 23:40:38 -0700

    build-docker-image.sh: upgrade Rocker to version 1.3.0

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,19 +3,21 @@ set -e
 
 MSG_PREFIX="[build-docker-image] >>>"
 COMMIT=${TRAVIS_COMMIT::8}
-ROCKER_VERSION=1.1.2
+ROCKER_VERSION=1.3.0
 
 # announcement.
 echo -e "${MSG_PREFIX} starting $0"
 
 # download rocker.
-curl -L https://github.com/grammarly/rocker/releases/download/${ROCKER_VERSION}/rocker-${ROCKER_VERSION}-linux_amd64.tar.gz | tar xfz - -C $HOME
+curl -L "https://github.com/grammarly/rocker/releases/download/${ROCKER_VERSION}/rocker_linux_amd64.tar.gz" \
+  | tar xfz - -C "${HOME}"
 
 # pass the auth if we are pushing to docker hub.
 EXTRA=""
-if [ -n "${DOCKER_USER}" -a -n "${DOCKER_PASS}" ]; then
+if [ -n "${DOCKER_USER}" ] && [ -n "${DOCKER_PASS}" ]; then
   EXTRA="--auth ${DOCKER_USER}:${DOCKER_PASS}"
 fi
 
 # build the image
-$HOME/rocker build --var BRANCH=$TRAVIS_BRANCH --var TAG=$TRAVIS_TAG --var COMMIT=$COMMIT --var TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER --push $EXTRA
+"${HOME}/rocker" build --var "BRANCH=${TRAVIS_BRANCH}" --var "TAG=${TRAVIS_TAG}" \
+  --var "COMMIT=${COMMIT}" --var "TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}" --push $EXTRA
