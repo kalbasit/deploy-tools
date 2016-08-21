5fe77bcc838644e0300a9fe05a7f602f83d7dbb0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2016-08-21 10:37:07 -0700

    kubectl-apply.sh: require KUBECONFIG_PATH directly

--- kubectl-apply.sh
+++ kubectl-apply.sh
@@ -7,12 +7,12 @@ MSG_PREFIX="[kubectl-apply.sh] >>>"
 echo -e "${MSG_PREFIX} starting $0"
 
 # sanity check
-if [[ -z "${AWS_TERRAFORM_REPO_PATH}" ]]; then
-  echo "${MSG_PREFIX} ${AWS_TERRAFORM_REPO_PATH} is empty, must be set to the path of the aws-terraform repository path"
+if [[ -z "${KUBECONFIG_PATH}" ]]; then
+  echo "${MSG_PREFIX} KUBECONFIG_PATH is empty, must be set to the path of kubeconfig."
   exit 1
 fi
-if [[ ! -d "${AWS_TERRAFORM_REPO_PATH}" ]]; then
-  echo "${MSG_PREFIX} ${AWS_TERRAFORM_REPO_PATH} does not exist, please clone it before calling this script"
+if [[ ! -d "${KUBECONFIG_PATH}" ]]; then
+  echo "${MSG_PREFIX} ${KUBECONFIG_PATH} does not exist."
   exit 1
 fi
 if [[ -z "${KUBE_MANIFEST_REPO_PATH}" ]]; then
@@ -27,17 +27,13 @@ if [[ -z "${KUBE_MANIFEST_FILES}" ]]; then
   echo "${MSG_PREFIX} KUBE_MANIFEST_FILES must be present."
   exit 1
 fi
-if [[ ${#KUBE_MANIFEST_FILES[@]} -eq 0 ]]; then
-  echo "${MSG_PREFIX} KUBE_MANIFEST_FILES must specify at least one file"
-  exit 1
-fi
 
 cd "${KUBE_MANIFEST_REPO_PATH}"
-for file in ${KUBE_MANIFEST_FILES[@]}; do
+for file in $(echo "${KUBE_MANIFEST_FILES}" | tr ":" "\n"); do
   if [[ ! -r ${file} ]]; then
     echo "${MSG_PREFIX} unable to write to ${file}"
     exit 1
   fi
   echo "${MSG_PREFIX} running 'kubectl apply -f ${file}'"
-  kubectl --kubeconfig="${AWS_TERRAFORM_REPO_PATH}/${KUBE_CONFIG}" apply -f "${file}"
+  kubectl --kubeconfig="${KUBECONFIG_PATH}" apply -f "${file}"
 done
